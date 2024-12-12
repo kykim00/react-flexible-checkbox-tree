30b4e46ba94d7e7beb657844af02d8d50a2a8b82
kykim00 <jeongsaeki@likelion.org>
2024-12-12 15:02:08 +0900

    feat: project setting

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+export default {
+    printWidth: 140,
+    tabWidth: 2,
+    useTabs: false,
+    semi: false,
+    singleQuote: true,
+    arrowParens: 'always',
+    rangeStart: 0,
+    endOfLine: 'auto',
+}
